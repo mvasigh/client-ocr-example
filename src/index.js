cbe7e6349777e46f9205bf4ce5c2418a7c3be8c3
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-08-01 11:33:36 -0500

    rasterize and render the PDF and capture the crop

--- src/index.js
+++ src/index.js
@@ -1,10 +1,63 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React, { useState, useRef, useEffect } from 'react';
+import { render } from 'react-dom';
+import {
+  readFile,
+  getPdfFromFile,
+  getImageFromPdfPage,
+  getSvgFromPdfPage
+} from './pdf-util';
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
 
 function App() {
+  const [file, setFile] = useState();
+  const [src, setSrc] = useState();
+  const [crop, setCrop] = useState({
+    unit: '%',
+    width: 30
+  });
+  const svg = useRef();
+
+  const handleFileChange = e => {
+    setFile(e.target.files[0]);
+  };
+
+  useEffect(() => {
+    if (!file) return;
+    readFile(file)
+      .then(fileArr => getPdfFromFile(fileArr))
+      .then(pdf => pdf.getPage(1))
+      .then(page => {
+        getImageFromPdfPage(page).then(image => setSrc(image));
+        getSvgFromPdfPage(page).then(svg => console.log({ svg }));
+      });
+  }, [file]);
+
   return (
-    <div>hello there</div>
-  )
+    <div
+      style={{
+        width: '100vw',
+        maxWidth: '100vw'
+      }}
+    >
+      <h1>Localized OCR example with Tesseract.js</h1>
+      <form>
+        <input type="file" onChange={handleFileChange}></input>
+      </form>
+      {src && (
+        <ReactCrop
+          style={{
+            maxWidth: '100vw'
+          }}
+          src={src}
+          crop={crop}
+          onImageLoaded={() => {}}
+          onComplete={a => console.log({ a })}
+          onChange={crop => setCrop(crop)}
+        />
+      )}
+    </div>
+  );
 }
 
-render(<App />, document.getElementById('app'))
+render(<App />, document.getElementById('app'));
---
+++ src/pdf-util.js
@@ -0,0 +1,79 @@
+import pdfjs from 'pdfjs-dist';
+
+// temporarily using a CDN for the worker until we figure out webpack mess
+pdfjs.GlobalWorkerOptions.workerSrc =
+  'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.1.266/pdf.worker.js';
+
+const MAX_DIMENSION_LENGTH = 6000;
+
+export function readFile(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = function() {
+      resolve(reader.result);
+    };
+
+    reader.readAsArrayBuffer(file);
+  });
+}
+
+export function getPdfFromFile(fileArr) {
+  return pdfjs.getDocument(fileArr).promise;
+}
+
+// returns a Promise that resolves to base64 encoded png
+export async function getImageFromPdfPage(page, scaleArg) {
+  // get width and height dimensions from page
+  const [, , w, h] = page.view;
+  // get scale based on max length
+  const scale = scaleArg || MAX_DIMENSION_LENGTH / Math.max(w, h);
+  const viewport = page.getViewport(scale);
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  canvas.height = viewport.height;
+  canvas.width = viewport.width;
+
+  const renderContext = {
+    canvasContext: context,
+    viewport
+  };
+
+  // return page.render(renderContext).then(() => canvas.toDataURL())
+  await page.render(renderContext);
+  return new Promise((resolve, reject) => {
+    canvas.toBlob(blob => {
+      if (!blob) {
+        //reject(new Error('Canvas is empty'));
+        console.error('Canvas is empty');
+        return;
+      }
+      blob.name = 'Diagram';
+      const fileUrl = window.URL.createObjectURL(blob);
+      resolve(fileUrl);
+    }, 'image/jpeg');
+  });
+}
+
+// export async function getSvgFromPdfPage(page) {
+//   const viewport = page.getViewport(1.0);
+//   const svgGfx = new pdfjs.SVGGraphics(page.commonObjs, page.objs);
+//   svgGfx.embedFonts = true;
+//   const operatorList = await page.getOperatorList();
+//   const svg = await svgGfx.getSVG(operatorList, viewport);
+//   return new Promise((resolve, reject) => {
+//     const img = new Image();
+//     // get svg data
+//     const xml = new XMLSerializer().serializeToString(svg);
+
+//     // make it base64
+//     const svg64 = btoa(xml);
+//     const b64Start = 'data:image/svg+xml;base64,';
+
+//     // prepend a "header"
+//     const image64 = b64Start + svg64;
+//     img.src = image64
+//     img.onload = () => resolve(img);
+//     img.onerror = e => reject(e);
+//   });
+// }
